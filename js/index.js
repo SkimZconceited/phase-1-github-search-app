2ab4450c8e86f0ea1a0a36064762a35bd15bea5f
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-20 11:55:37 +0300

    Completed Assignment

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 let url = 'https://api.github.com/search/users';
 
+let searchBox = document.getElementById('search');
+let btnSub = document.querySelector('input[type = "submit"]');
+let searchForm = document.getElementById('github-form');
+let userList = document.getElementById('user-list');
+let repoList = document.getElementById('repos-list');
+
+
 let user = null;
-let repos = [];
-let li = document.createElement('li');
 
 function fetchResults(search) {
     fetch(`${url}?q=${search}`)
@@ -10,17 +15,21 @@ function fetchResults(search) {
     .then(data => data);
 }
 
+function appendList (name) {
+    repoList.innerHTML += `<p><a href="https://github.com/${user}/${name}" target="_blank">${name}</a></p>`
+}
+
 function getRepo(user) {
     fetch(`https://api.github.com/users/${user}/repos`)
     .then(res => res.json())
-    .then(data => data.forEach(repo => {repos.push(repo.name)}));
+    .then(data => data.forEach(repo => {
+        // repoList.textContent = repo.name;
+        appendList(repo.name);
+        console.log(repo)
+    }));
 }
 
 // fetchResults('octocat')
-
-let searchBox = document.getElementById('search');
-let btnSub = document.querySelector('input[type = "submit"]');
-let searchForm = document.getElementById('github-form');
 
 btnSub.addEventListener('click', () => {
     if (!searchBox.value) {return};
@@ -28,20 +37,9 @@ btnSub.addEventListener('click', () => {
     user = searchBox.value;
     console.log(user)
 
-    let userList = document.getElementById('user-list');
-    let repoList = document.getElementById('repos-list');
 
     userList.textContent = user.toUpperCase();
     getRepo(user);
-
-    // for (i = 0; i < repos.length; i++) {
-    //     repoList.li.textContent = repos[i];
-    // }
-    console.log(repos)
-
-    // searchBox.value = '';
 })
 
 searchForm.addEventListener('submit', (e) => e.preventDefault());
-// window.setTimeout(console.log('Btn Clickedx'), 2000)
-
